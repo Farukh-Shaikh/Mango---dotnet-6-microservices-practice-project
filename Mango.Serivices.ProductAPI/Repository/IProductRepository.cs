c214f4ffc880f256606ec4e1c91f17439d4222df
Farukh Ahmed <farukh.ahmed@techlogix.com>
2024-02-17 17:56:26 +0500

    Mango practice project for microservices

---
+++ Mango.Serivices.ProductAPI/Repository/IProductRepository.cs
@@ -0,0 +1,12 @@
+﻿using Mango.Serivices.ProductAPI.Models;
+
+namespace Mango.Serivices.ProductAPI.Repository
+{
+    public interface IProductRepository
+    {
+        Task<IEnumerable<ProductDto>> GetProducts();
+        Task<ProductDto> GetProductById(int productId);
+        Task<ProductDto> CreateUpdateProduct(ProductDto productDto);
+        Task<bool> DeleteProduct(int productId);
+    }
+}
